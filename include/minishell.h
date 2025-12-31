51ab3c37d94bc4d802fc7e6ba9eeb70e5abf75a4
Jazmin Acosta <jaezatc@gmail.com>
2025-12-31 08:47:02 +0100

    feat(parser): adding AST struct

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mariacos <mariacos@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 17:05:57 by mariacos          #+#    #+#             */
-/*   Updated: 2025/12/28 23:00:28 by mariacos         ###   ########.fr       */
+/*   Updated: 2025/12/31 08:20:46 by mariacos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,12 +44,35 @@ typedef enum s_token_type
 	T_EOF		// nomo input to read 
 }	t_token_type;
 
+typedef enum s_node_type
+{
+	NODE_COMMAND,
+	NODE_PIPE
+}	t_node_type;
+
+// redir only for the tree
+typedef enum s_redir_type
+{
+	REDIR_IN,       // 
+	REDIR_OUT,      // >
+	REDIR_APPEND,   // >>
+	REDIR_HEREDOC   // 
+}	t_redir_type;
+
 typedef struct s_token
 {
 	t_token_type type;
 	char *value;
 	struct s_token *next;
 }	t_token;
+
+//  Redir struct for the tree
+typedef struct s_redir
+{
+	t_redir_type	type;
+	char			*file;
+	struct s_redir	*next;
+}	t_redir;
 
 typedef struct s_data
 {
