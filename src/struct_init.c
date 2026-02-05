ab9022e1f440a69132b2d71adbec79a603448705
Jazmin Acosta <jaezatc@gmail.com>
2026-02-05 16:09:42 +0000

    Parser: struct finished

--- src/struct_init.c
+++ src/struct_init.c
@@ -63,3 +63,54 @@ t_cmd *create_struct(t_minishell *data)
     free(st);
     return (result);
 }
+
+void add_redir_node(t_cmd *cmd, t_token_type type, char *expanded_file)
+{
+    t_redir *current_redir;
+    t_redir *last;
+
+    current_redir = malloc(sizeof(t_redir));
+    if (!current_redir)
+        return ;
+    current_redir->type = type;
+    current_redir->file = ft_strdup(expanded_file);
+    current_redir->next = NULL;
+    if (!cmd->redirs) 
+        cmd->redirs = current_redir;
+    else
+    {
+        last = cmd->redirs;
+        while (last->next)
+            last = last->next;
+        last->next = current_redir;
+    }
+}
+
+char *remove_quotes(char *str)
+{
+	int i;
+	int j;
+	char in_quote;
+	char *str_clean;
+
+	i = 0;
+	j = 0;
+	in_quote = 0;
+	if(!str)
+		return (NULL);
+	str_clean = malloc(sizeof(char) * (ft_strlen(str) + 1));
+	if(!str_clean)
+		return (NULL);
+	while(str[i])
+	{
+		if((str[i] == '\'' || str[i] == '\"') && in_quote == 0)
+			in_quote = str[i];
+		else if(str[i] == in_quote)
+			in_quote = 0;
+		else
+			str_clean[j++] = str[i];
+		i++;
+	}
+	str_clean[j] = '\0';
+	return (str_clean);
+}
