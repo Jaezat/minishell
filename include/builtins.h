231e1d81226fdeeed6639409a3f1413cea6bd4d4
acmmarques <andremarques94@gmail.com>
2026-02-25 16:00:34 +0000

    Organization: created header files for all different parts of execution

---
+++ include/builtins.h
@@ -0,0 +1,16 @@
+#ifndef BUILTINS_H
+# define BUILTINS_H
+
+int		validate_shell_variable(char *str);
+t_env	*create_env_from_key_and_value(char *key, char *value);
+void	extract_key_and_value_from_arg(char *arg, char **key, char **value);
+void	upsert_env_var(t_minishell *shell, char *arg);
+int		ft_echo(char **args);
+int		ft_cd(t_minishell *shell, char **args);
+int		ft_pwd(void);
+int		ft_export(t_minishell *shell, char **args);
+int		ft_unset(t_minishell *shell, char **args);
+int		ft_env(t_env *env);
+int		ft_exit(t_minishell *shell, char **args);
+
+#endif
